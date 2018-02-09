1919fb4fb3006dff0d6e0ae0c8bb8188b9f335e8
Heather Gallop <doyouseeitmyway@gmail.com>
2018-02-09 09:04:47 -0500

    Add TextViews and Checkboxes for first question. Adjust styling. Add logic for submit name button to display a toast.

--- app/src/main/java/com/example/android/alzheimersawarenessquiz/MainActivity.java
+++ app/src/main/java/com/example/android/alzheimersawarenessquiz/MainActivity.java
@@ -2,14 +2,19 @@ package com.example.android.alzheimersawarenessquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private EditText nameText;
     private TextView userName;
+    private Button questionOne;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
 
         nameText = findViewById(R.id.name_text);
         userName = findViewById(R.id.user_name);
+        questionOne = findViewById(R.id.question_one);
     }
 
 
@@ -31,5 +37,8 @@ public class MainActivity extends AppCompatActivity {
     public void nameText(View view) {
         String name = nameText.getText().toString();
         displayName(name);
+        Toast.makeText(this, this.getString(R.string.toastScroll), Toast.LENGTH_SHORT).show();
     }
+
+
 }
